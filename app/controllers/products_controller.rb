f4e7d32ba8fe4e90ff8aa1db2b77721d5407c332
Ruchika Mayekar <ruchika.mayekar@kiprosh.com>
2014-12-11 17:05:41 +0530

    Initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+  def  show
+    @product = Product.find(params[:id])
+  end
+  def  index
+    @products = Product.all
+  end
+  def  new
+    @product = Product.new
+  end
+  def  create
+    @product = Product.new(product_values)
+    @product.save
+    redirect_to products_path(@product)
+  end
+  def edit
+    @product = Product.find(params[:id])
+  end
+  def  update
+    @product = Product.find(params[:id])
+    @product.update_attributes(product_values)
+    redirect_to product_path(@product.id)
+  end
+
+  def  destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+  def product_values
+    params.require(:product).permit(:title, :description, :img_url, :price)
+  end
+end
---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ActiveRecord::Base
+  validates :title, :description, :img_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :img_url, format: {
+with: %r{\.(gif|jpg|png)\z}i, message: 'must be a URL for GIF, JPG or PNG image.'
+}
+end
