fdcde719319b4cb091105482e5d0b62065d05386
Ruchika Mayekar <ruchika.mayekar@kiprosh.com>
2014-12-15 11:34:08 +0530

    made some changes

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  def increment_count
+  session[:counter] ||= 0
+  session[:counter] += 1
+  end
   def index
     @products = Product.all
+    @count = increment_count
   end
 end
